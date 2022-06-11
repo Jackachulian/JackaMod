3232882ec67dc86454b839f28e288efbcd00d47e
Jack <jackachulian@outlook.com>
2022-06-11 14:32:01 -0500

    Setup the project

---
+++ src/main/java/net/jackachulian/jackamod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.jackachulian.jackamod.mixin;
+
+import net.jackachulian.jackamod.JackaMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		JackaMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
